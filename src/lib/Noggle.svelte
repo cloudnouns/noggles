56d48c957b3471c26ff5a4587e1a300bcbebcacb
Badu Blanc <badu@hey.com>
2022-07-27 17:02:30 -0500

    initial commit

---
+++ src/lib/Noggle.svelte
@@ -0,0 +1,27 @@
+<script>
+	import opts from '$lib/noggle.config.ts';
+	import { animate } from 'motion';
+
+	export let fixed = false;
+	export let frameColor = '#e8705b';
+	export let flipEyes = false;
+
+	const position = fixed ? opts.fixed : opts.box;
+	const { viewBox, frames, leftEye, rightEye } = position;
+
+	let leftEyeColors = ['#fff', '#000'];
+	let rightEyeColors = ['#fff', '#000'];
+
+	if (flipEyes) {
+		leftEyeColors.reverse();
+		rightEyeColors.reverse();
+	}
+</script>
+
+<svg xmlns="http://www.w3.org/2000/svg" {viewBox} fill="none">
+	<path fill-rule="evenodd" clip-rule="evenodd" d={leftEye.white} fill={leftEyeColors[0]} />
+	<path fill-rule="evenodd" clip-rule="evenodd" d={leftEye.pupil} fill={leftEyeColors[1]} />
+	<path fill-rule="evenodd" clip-rule="evenodd" d={rightEye.white} fill={rightEyeColors[0]} />
+	<path fill-rule="evenodd" clip-rule="evenodd" d={rightEye.pupil} fill={rightEyeColors[1]} />
+	<path fill-rule="evenodd" clip-rule="evenodd" d={frames} fill={frameColor} />
+</svg>
---
+++ src/routes/index.svelte
@@ -0,0 +1,15 @@
+<script>
+	import { Noun } from '@cloudnouns/kit';
+	import Noggle from '$lib/Noggle.svelte';
+
+	let noun = new Noun({ traits: { glasses: 'n' } });
+</script>
+
+<div class="flex items-center justify-center min-h-screen bg-[#dcff70]">
+	<div class="w-[500px] h-[500px] relative border-4 border-black">
+		<div class="w-full absolute top-0 left-0">
+			<Noggle fixed />
+		</div>
+		<img src={noun.images.svg} alt="" class="w-full" />
+	</div>
+</div>
